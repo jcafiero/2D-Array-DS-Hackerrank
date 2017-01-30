6c2f128b3056ad6a6c35a40d534b58c69e42cdfa
Jennifer Cafiero <jcafiero@stevens.edu>
2017-01-30 18:58:33 -0500

    Create HackerRank2DArray.java

---
+++ HackerRank2DArray.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        for(int arr_i=0; arr_i < 6; arr_i++){
+            for(int arr_j=0; arr_j < 6; arr_j++){
+                arr[arr_i][arr_j] = in.nextInt();
+            }
+        }
+        int max = arr[0][0] + arr[0][1] + arr[0][2] + arr[1][1] + arr[2][0] + arr[2][1] + arr[2][2];
+     
+        for(int i = 0; i < 4; i++) 
+        {
+            for(int j = 0; j < 4; j++) 
+            {
+                int temp_max= arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+                if (temp_max > max) {
+                    max = temp_max;
+                    
+                }
+                           
+            }
+        }
+        System.out.print(max);
+    }
+}
